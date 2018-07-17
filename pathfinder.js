498f4bbc95335dac45cf21b2f5a5560d6a07cf73
Andrey <gman007@yandex.ru>
2018-07-17 12:18:24 +0400

    js implementation of pathfiding

--- pathfinder.js
+++ pathfinder.js
@@ -1,5 +1,72 @@
-function pathfinder() {
-    console.log("Dick");
+const fs = require('fs');
+
+class PathFinder {
+    constructor (pathToGraph) {
+        this.graph = JSON.parse(fs.readFileSync(pathToGraph))
+    }
+
+    findPath(startNode, finishNode) {
+        const nodePotentials = []
+        nodePotentials.push(new Node(startNode, 0));
+        const graphPath = {};
+        const nodesCost = {};
+        graphPath[startNode] = null
+        nodesCost[startNode] = 0;
+        while (nodePotentials.length != 0) {
+            let currentNode = this._popMostPotentialNode(nodePotentials).nodeName
+            if (currentNode == finishNode) {
+                break;
+            }
+
+            Object.entries(this.graph[currentNode]).forEach((entry) => {
+                let nodeName = entry[0]
+                let pathCost = entry[1]
+                let newCost = nodesCost[currentNode] + pathCost;
+                if (!(nodeName in graphPath) || newCost < nodesCost[nodeName]) {
+                    nodesCost[nodeName] = newCost;
+                    nodePotentials.push(new Node(nodeName, newCost));
+                    graphPath[nodeName] = currentNode
+                }
+            })
+        }
+
+        return new PathfindingResult(this._buildPath(graphPath, startNode, finishNode), nodesCost[finishNode]);
+    }
+
+    _popMostPotentialNode(nodePotentials) {
+        let mostPotentialNode = nodePotentials.reduce((currMin, currValue) => currMin.nodePathSum < currValue.nodePathSum 
+                                                        ? currMin 
+                                                        : currValue)
+        nodePotentials.splice(nodePotentials.indexOf(mostPotentialNode), 1);
+        return mostPotentialNode;
+    }
+
+    _buildPath(graphPath, startNode, finishNode) {
+        let currentNode = finishNode;
+        const path = [];
+        while (currentNode != startNode) {
+            path.push(currentNode);
+            currentNode = graphPath[currentNode];
+        }
+        path.push(currentNode);
+        path.reverse();
+        return path;
+    }
 }
 
-pathfinder();
+class Node {
+    constructor(nodeName, nodePathSum) {
+        this.nodeName = nodeName;
+        this.nodePathSum = nodePathSum;
+    }
+}
+
+class PathfindingResult {
+    constructor(path, pathCost) {
+        this.path = path;
+        this.pathCost = pathCost;
+    }
+}
+
+var pathFinder = new PathFinder('graph.json');
+console.log(pathFinder.findPath('1', '5'))
